b97a17f43cd7d4a74d5352d6a7ace520d31c938d
Максим <56961609+PhoenixInDarkness@users.noreply.github.com>
2023-09-22 23:13:36 +0300

    Delete app directory

--- app/routes/social.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Laravel\Socialite\Facades\Socialite;
-use App\Http\Controllers\Auth\SocialController;
-
-Route::get('/auth/google/redirect', [SocialController::class, 'googleRedirect']);
-
-Route::get('/auth/google/callback', [SocialController::class, 'googleCallBack']);
